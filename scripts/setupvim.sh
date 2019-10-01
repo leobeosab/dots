4a74b5350e62462f7932b1466173692b51408a92
Ryan Wise <ryan@ryanwise.me>
2019-10-01 00:24:41 +0000

    add vim setup

---
+++ scripts/setupvim.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
