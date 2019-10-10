adc4c64335fbe0d7a009842fd2944e086d513cd9
Ryan Wise <ryan@ryanwise.me>
2019-10-10 23:41:56 +0000

    added copy background + rofi backup

--- utils/copy_dots.sh
+++ utils/copy_dots.sh
@@ -5,3 +5,4 @@ cp -r $HOME/.config/rofi $HOME/dots/base/.config/
 rm $HOME/dots/base/.config/rofi/config.rasi
 
 cp -r $HOME/.config/i3 $HOME/.config/rofi $HOME/.config/polybar $HOME/.config/termite $HOME/.config/compton $HOME/dots/themes/$1/.config
+cp $HOME/.background $HOME/dots/themes/$1/.background
