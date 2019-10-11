752b5d8119d996f82bfd3b4150f409c8bb0c5218
Ryan Wise <ryan@ryanwise.me>
2019-10-11 00:25:25 -0400

    polybar on multiple monitors script

--- themes/synthwave/.config/polybar/launch.sh
+++ themes/synthwave/.config/polybar/launch.sh
@@ -9,4 +9,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini main &
+  done
+else
+  polybar --reload -c ~/.config/polybar/config.ini main
+fi
