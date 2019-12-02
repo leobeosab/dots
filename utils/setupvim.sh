980680843bae8cbddac30130b1e5c43906a0bd1b
Ryan <ryan@ryanwise.me>
2019-12-01 20:11:27 -0500

    updates to compton remove setupvim

--- utils/setupvim.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
